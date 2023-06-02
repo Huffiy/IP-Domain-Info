a0b9a7b3781e328bd1054bee07c22fb99aa1edac
Huffiy <mail@huffiy.me>
2023-06-02 08:45:29 +0300

    Update main.py

--- main.py
+++ main.py
@@ -11,25 +11,14 @@ access_token = 'YOUR_TOKEN_HERE'
 
 handler = ipinfo.getHandler(access_token)
 
-def valid_IP_Address(sample_str):
-    result = True
-    match_obj = re.search( r"^(\d{1,3})\.(\d{1,3})\.(\d{1,3})\.(\d{1,3})$", sample_str)
-    if  match_obj is None:
-        result = False
-    else:
-        for value in match_obj.groups():
-            if int(value) > 255:
-                result = False
-                break
-    return result
-
-
-if (input == True) :
-    details = handler.getDetails(input)
+if (input == None):
+    details = handler.getDetails()
 else:
-    ipOfDomain = socket.gethostbyname_ex(input)
-    details = handler.getDetails(ipOfDomain[2][0])
-
+    if (input == True) :
+        details = handler.getDetails(input)
+    else:
+        ipOfDomain = socket.gethostbyname_ex(input)
+        details = handler.getDetails(ipOfDomain[2][0])
 
 for key, value in details.all.items():
     print(f"{key}: {value}")
